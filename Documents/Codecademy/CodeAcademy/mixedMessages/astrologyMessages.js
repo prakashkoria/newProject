a7153e52012f66e8dc29d9ce8299d22ecd26aff7
Kash <prakashkoria@hotmail.com>
2023-10-21 14:53:42 +0100

    added messages for the rest of the astrology signs

--- Documents/Codecademy/CodeAcademy/mixedMessages/astrologyMessages.js
+++ Documents/Codecademy/CodeAcademy/mixedMessages/astrologyMessages.js
@@ -46,5 +46,145 @@ const zodiacSigns = [
           "Your well-being is a top priority."
         ]
       },
+      Gemini: {
+        Love: [
+          "Communication is key in your love life today.",
+          "Engage in deep conversations with your partner."
+        ],
+        Career: [
+          "Adaptability will be your strong suit at work.",
+          "Embrace change and new challenges."
+        ],
+        Health: [
+          "Stay active and keep your mind sharp today.",
+          "Physical and mental health are interconnected."
+        ]
+      },
+      Cancer: {
+        Love: [
+          "Your emotions are in harmony with your partner's.",
+          "Spend quality time with your loved one."
+        ],
+        Career: [
+          "You'll excel in nurturing roles at work.",
+          "Embrace your caring and supportive nature."
+        ],
+        Health: [
+          "Focus on emotional well-being today.",
+          "Nurture your soul and find balance."
+        ]
+      },
+      Leo: {
+        Love: [
+          "You're radiating charisma and attracting love.",
+          "Be open to new romantic adventures."
+        ],
+        Career: [
+          "Your leadership skills shine at work.",
+          "Take charge and lead with confidence."
+        ],
+        Health: [
+          "Indulge in self-care and relaxation today.",
+          "Recharge and pamper yourself."
+        ]
+      },
+      Virgo: {
+        Love: [
+          "Pay attention to the details in your relationship.",
+          "Small gestures of love go a long way."
+        ],
+        Career: [
+          "Organize and streamline your tasks at work.",
+          "Efficiency is your key to success."
+        ],
+        Health: [
+          "Maintain a balanced diet and stay active.",
+          "Healthy habits are your foundation."
+        ]
+      },
+      Libra: {
+        Love: [
+          "Harmony and balance prevail in your love life.",
+          "Embrace the beauty of your relationships."
+        ],
+        Career: [
+          "Diplomacy and cooperation lead to success.",
+          "Find common ground with colleagues."
+        ],
+        Health: [
+          "Prioritize mental and emotional well-being.",
+          "Inner balance is your strength."
+        ]
+      },
+      Scorpio: {
+        Love: [
+          "Intense emotions fuel your love life today.",
+          "Open up and share your deepest feelings."
+        ],
+        Career: [
+          "Your determination leads to career breakthroughs.",
+          "Face challenges with unwavering resolve."
+        ],
+        Health: [
+          "Explore alternative health practices today.",
+          "Rejuvenate your mind and body."
+        ]
+      },
+      Sagittarius: {
+        Love: [
+          "Adventure awaits in your love life.",
+          "Embrace spontaneity and exploration."
+        ],
+        Career: [
+          "Your enthusiasm is infectious at work.",
+          "Inspire and motivate your team."
+        ],
+        Health: [
+          "Engage in outdoor activities for vitality.",
+          "Nature rejuvenates your spirit."
+        ]
+      },
+      Capricorn: {
+        Love: [
+          "Stability and commitment are key in love.",
+          "Build a solid foundation with your partner."
+        ],
+        Career: [
+          "Hard work and discipline yield rewards.",
+          "Set ambitious goals and stay focused."
+        ],
+        Health: [
+          "A structured routine is essential for well-being.",
+          "Balance work and relaxation."
+        ]
+      },
+      Aquarius: {
+        Love: [
+          "Unconventional love approaches lead to excitement.",
+          "Embrace your uniqueness in relationships."
+        ],
+        Career: [
+          "Innovation and creativity shine at work.",
+          "Think outside the box and be bold."
+        ],
+        Health: [
+          "Mental clarity is crucial for overall health.",
+          "Stay true to your intellectual pursuits."
+        ]
+      },
+      Pisces: {
+        Love: [
+          "Connect on a deep emotional level with your partner.",
+          "Intuition guides your romantic choices."
+        ],
+        Career: [
+          "Express your creativity in your work.",
+          "Trust your artistic talents."
+        ],
+        Health: [
+          "Nurture your spiritual well-being today.",
+          "Embrace meditation and relaxation."
+        ]
+      }
 }
   
