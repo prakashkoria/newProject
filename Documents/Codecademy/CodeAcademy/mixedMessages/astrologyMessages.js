f961ea9663664a4e67792fc20b86e2dcb62c1bfe
Kash <prakashkoria@hotmail.com>
2023-10-21 13:35:26 +0100

    added atrology messages for Aries

--- Documents/Codecademy/CodeAcademy/mixedMessages/astrologyMessages.js
+++ Documents/Codecademy/CodeAcademy/mixedMessages/astrologyMessages.js
@@ -17,4 +17,20 @@ const zodiacSigns = [
 
   const horoscopeElements = ["Love", "Career", "Health"];
 
+  const astrologyMessages = {
+    Aries: {
+      Love: [
+        "Your love life is about to get more exciting.",
+        "Expect passion and romance today."
+      ],
+      Career: [
+        "You'll have a productive day at work.",
+        "Opportunities for advancement are on the horizon."
+      ],
+      Health: [
+        "It's a good day to focus on your physical well-being.",
+        "Listen to your body and take care of yourself."
+      ]
+    },
+}
   
