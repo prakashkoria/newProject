9b0b575a1c3a406d20e4c7a4ecbc1f795225b8e4
Kash <prakashkoria@hotmail.com>
2023-10-21 13:25:26 +0100

    create Zodiac Signs array

---
+++ Documents/Codecademy/CodeAcademy/mixedMessages/astrologyMessages.js
@@ -0,0 +1,16 @@
+//create array for the 12 Zodiac signs
+
+const zodiacSigns = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces",
+  ];
